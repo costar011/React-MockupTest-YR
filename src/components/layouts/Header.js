75f900182239cb457c7086a5000afcd96c8cc7c0
YERIM <dnaer456@gmail.com>
2020-10-16 23:22:47 +0900

    header

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">Header</div>
+    );
+  }
+}
+
+
+export default Header;
