0ec7348d11458b66ec2f7db46745aa5f9b8dc7d0
YERIM <dnaer456@gmail.com>
2020-10-16 23:33:40 +0900

    hedaer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Header } from "./components/layouts";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    return<div>hi</div>
+    return (
+      <div>
+        <header>
+          <Route path="/" component={Header} />
+        </header>
+      </div>
+    );
   }
 }
-
-
 
 export default App;
---
+++ src/components/Screens/MM00.js
@@ -0,0 +1,8 @@
+import Raect from "react";
+
+class MM00 extends React.Component {
+    render() {
+        return <div className="MM00">MM00</div>
+    }
+}
+export default MM00;
