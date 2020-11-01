0cff6b5032f6c17d56597c2ac68e3e11dd65d18b
YERIM <dnaer456@gmail.com>
2020-11-01 10:40:51 +0900

    img

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import mockup from "../image/mockup.png";
 
 class Header extends React.Component {
   render() {
     return (
       <div className="Header">
         <div className="Header__title">AGAIN</div>
+
         <div className="Hedaer__main">
           <div className="Header__main1">Movie</div>
+          <div className="Header__mockup">
+            <img src={mockup} />
+          </div>
         </div>
       </div>
     );
