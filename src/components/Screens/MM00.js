34e48eb116a6ff29d5c6f6cc1821c76b41a7f63f
YERIM <dnaer456@gmail.com>
2020-11-04 19:20:51 +0900

    MM00 css

--- src/components/Screens/MM00.js
+++ src/components/Screens/MM00.js
@@ -5,16 +5,13 @@ class MM00 extends React.Component {
   render() {
     return (
       <div className="MM00 screen">
-        <div className="MM00__main">
-          <div className="MM00__txt">
-            <div className="MM00__txt1"></div>
-            <div className="MM00__txt2">MM00 Page</div>
-          </div>
+        <div className="MM00__txt">
+          <div className="MM00__txt1">MM00</div>
+          <div className="MM00__txt2">Page</div>
+        </div>
 
-          <div className="MM00__mockup">
-            <img src={mockup} />
-            <div className="mockup__img"></div>
-          </div>
+        <div className="MM00__mockup">
+          <img src={mockup} />
         </div>
       </div>
     );
