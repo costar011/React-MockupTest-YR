022105d2a03e2ff12792e79c4c54c2ecda0fcca7
YERIM <dnaer456@gmail.com>
2020-11-09 22:38:55 +0900

    MM01

--- src/components/Screens/MM01.js
+++ src/components/Screens/MM01.js
@@ -2,7 +2,18 @@ import React from "react";
 
 class MM01 extends React.Component {
   render() {
-    return <div className="MM01 screen">MM01</div>;
+    return (
+      <div className="MM01 screen">
+        <div className="MM01__txt">
+          <div className="MM01__txt1">txt1</div>
+          <div className="MM01__txt2">txt2</div>
+        </div>
+
+        <div className="MM01__mockup">
+          <img src={mockup} />
+        </div>
+      </div>
+    );
   }
 }
 export default MM01;
