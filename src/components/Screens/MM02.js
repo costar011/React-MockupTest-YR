0f279bf07cd181d210bdf6f64c8e37f963ebed37
YERIM <dnaer456@gmail.com>
2020-11-16 19:52:01 +0900

    MM02

--- src/components/Screens/MM02.js
+++ src/components/Screens/MM02.js
@@ -3,7 +3,14 @@ import mockup from "../image/mockup.png";
 
 class MM02 extends React.Component {
   render() {
-    return <div className="MM02 screen"></div>;
+    return (
+      <div className="MM02 screen">
+        <div className="MM02__txt">
+          <div className="MM02__txt1">txt1</div>
+          <div className="MM02__txt2">txt2</div>
+        </div>
+      </div>
+    );
   }
 }
 export default MM02;
