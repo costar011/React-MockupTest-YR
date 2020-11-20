8ca7fa896ae7cd1d20d1d88a00a9eee86c2323f6
YERIM <dnaer456@gmail.com>
2020-11-20 12:09:46 +0900

    MM03 mockup

--- src/components/Screens/MM03.js
+++ src/components/Screens/MM03.js
@@ -3,7 +3,18 @@ import mockup from "../image/mockup.png";
 
 class MM03 extends React.Component {
   render() {
-    return <div className="MM03 screen"></div>;
+    return (
+      <div className="MM03 screen">
+        <div className="MM03__txt">
+          <div className="MM03__txt1">MM03</div>
+          <div className="MM03__txt2">Page</div>
+        </div>
+
+        <div className="MM02__mockup">
+          <img src={mockup} />
+        </div>
+      </div>
+    );
   }
 }
 export default MM03;
